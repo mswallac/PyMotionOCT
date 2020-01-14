411cff1613fe303cc7769d06ab8d1aa45ce0e11b
mswallac <mswallac@bu.edu>
2020-01-14 15:22:20 -0500

    Update alineprocessor.py

--- alineprocessor.py
+++ alineprocessor.py
@@ -18,19 +18,18 @@ import matplotlib.pyplot as plt
 
 class AlineProcessor:
     
-    def __init__(self,localsize):
+    def __init__(self,data):
         
         # load wavelength array, hanning window, linear in wavenumber interpolation prereqs.
-        lam = np.load('lam.npy')
-        win = np.hanning(2048).astype(np.float16)
+        lam = np.load('lam.npy').astype(np.float32)
+        win = np.hanning(2048).astype(np.float32)
         lam_min = np.amin(lam)
         lam_max = np.amax(lam)
-        d_lam = np.asarray([lam_max-lam_min])
+        d_lam = np.asarray([lam_max-lam_min]).astype(np.float32)
         d_k = (1/lam_min - 1/lam_max)/2048
-        k = np.array([1/((1/lam_max)+d_k*i) for i in range(2048)])
-        nn0 = np.zeros((2048,),int)
-        nn1 = np.zeros((2048,),int)
-        
+        k = np.array([1/((1/lam_max)+d_k*i) for i in range(2048)]).astype(np.float32)
+        nn0 = np.zeros((2048,),np.int32)
+        nn1 = np.zeros((2048,),np.int32)
         # find nearest neighbors for each point in the lambda spec for interp prep.
         for i in range(1,2047):
             res = np.abs(lam-k[i])
@@ -41,11 +40,11 @@ class AlineProcessor:
             else:
                 nn0[i]=minind
                 nn1[i]=minind+1
-                
+        
         self.nn0_np=nn0
         self.nn1_np=nn1
         # do preliminary setup for opencl side of things (platform,device,context,queue)
-        self.dtype = np.complex64
+        self.dtype = np.float32
         self.platform = cl.get_platforms()
         self.platform = self.platform[0]
         self.device = self.platform.get_devices()
@@ -61,12 +60,13 @@ class AlineProcessor:
         self.lam = cl.Buffer(self.context, mflags.READ_ONLY | mflags.COPY_HOST_PTR, hostbuf=lam)
         self.d_lam = cl.Buffer(self.context, mflags.READ_ONLY | mflags.COPY_HOST_PTR, hostbuf=d_lam)
         
-        self.npdata = np.zeros((2048))
-        self.npres = np.zeros((2048))
+        self.npdata = np.zeros((2048,)).astype(np.float32)
+        self.npres = np.zeros((2048,)).astype(np.float32)
+        self.npres1 = np.zeros((2048,)).astype(np.float32)
         self.result_ih = cl.Buffer(self.context, cl.mem_flags.COPY_HOST_PTR, hostbuf=self.npres)
-        self.result_nan = cl.Buffer(self.context, cl.mem_flags.COPY_HOST_PTR, hostbuf=self.npres)
+        self.result_nan = cl.Buffer(self.context, cl.mem_flags.COPY_HOST_PTR, hostbuf=self.npres1)
         self.global_wgsize = (2048,)
-        self.local_wgsize = (localsize,)
+        self.local_wgsize = (512,)
         self.data = cl.Buffer(self.context, mflags.COPY_HOST_PTR, hostbuf=self.npdata)
         self.result = cl.Buffer(self.context, mflags.COPY_HOST_PTR, hostbuf=self.npres)
         
@@ -74,40 +74,42 @@ class AlineProcessor:
         self.api = cluda.ocl_api()
         self.thr = self.api.Thread.create()
         self.dshape = (2048)
-        arbdata = self.thr.to_device(np.zeros(self.dshape).astype(self.dtype))
-        self.outp = self.thr.array(self.dshape,self.dtype)
+        arbdata = self.thr.to_device(np.zeros(self.dshape).astype(np.complex64))
+        self.outp = self.thr.array(self.dshape,np.complex64)
         self.fft = FFT(arbdata,axes=(0,)).compile(self.thr)
+        
         self.program = cl.Program(self.context, """
-        __kernel void interp_hann(global float *lam_s, global float *lam, global float *k, global int *nn0,
-                                  global int *nn1, global float *d_lam, global float *win, global float *res)
+        __kernel void hann(__global float *lam_s, __global float *win, __global float *res)
+        {
+            int i = get_global_id(0);
+            res[i] = lam_s[i]*win[i];
+        }
+        
+        __kernel void interp(__global float *lam_s, __global float *lam, __global float *k, __global int *nn0,
+                                  __global int *nn1, __global float *d_lam, __global float *win, __global float *res)
         {
             int i = get_global_id(0);
             
-    		if ((lam_s[nn0[i]] - lam_s[nn1[i]]) < 1E-12)
+            float y1 = lam_s[nn0[i]];
+            float y2 = lam_s[nn1[i]];
+            float x1 = k[i];
+            float dl = d_lam[0];
+            float x2 = x1+dl;
+            
+    		if (y1==y2)
             {
-    			res[i] = lam_s[nn0[i]]*win[i];
+    			res[i] = y1;
     		}
     		else
     		{
-    			res[i] = (lam_s[nn0[i]] + (k[i] - lam[nn1[i]])/(lam_s[nn0[i]] - lam_s[nn1[i]]))*d_lam[0]*win[i];
+    			res[i] = (y1 + (x1 - x2)/(y1 - y2))*dl;
     		}
 
         }
-        __kernel void replace_nan(global float *input, global float *res)
-        {
-            int i = get_global_id(0);
-            float val = input[i];
-            
-            if (isnan(val))
-            {
-                res[i]=1;
-            }
-            else
-            {
-                res[i]=val;
-            }
-        }
         """).build()
+        
+        self.interp = self.program.interp
+        self.hann = self.program.hann
         
     def reikna_FFT(self,data):
         inp = self.thr.to_device(data)
@@ -115,13 +117,19 @@ class AlineProcessor:
         return self.outp.get()
         
     def interp_hann_wrapper(self,data):
+        #apply hanning window
         self.data = cl.Buffer(self.context, cl.mem_flags.COPY_HOST_PTR, hostbuf=data)
-        self.program.interp_hann(self.queue,self.global_wgsize,self.local_wgsize,self.data,self.lam,self.k,self.nn0,self.nn1,
-                                     self.d_lam,self.win,self.result_ih)
-        self.program.replace_nan(self.queue,self.global_wgsize,self.local_wgsize,self.result_ih,self.result_nan)
-        cl.enqueue_copy(self.queue,self.npres,self.result_nan)
-        return self.npres
-        return
+        self.hann.set_args(self.data,self.win,self.result_ih)
+        cl.enqueue_nd_range_kernel(self.queue,self.hann,self.global_wgsize,self.local_wgsize)
+        
+        #do interpolation
+        self.interp.set_args(self.result_ih,self.lam,self.k,self.nn0,self.nn1,self.d_lam,self.win,self.result_nan)
+        cl.enqueue_nd_range_kernel(self.queue,self.interp,self.global_wgsize,self.local_wgsize)
+        cl.enqueue_copy(self.queue,self.npres1,self.result_nan)
+        
+        plt.plot(self.npres1)
+        
+        return self.npres1
     
     def process_aline(self,data):
         self.interp_hann_wrapper(data)
@@ -130,26 +138,11 @@ class AlineProcessor:
 
 if __name__ == '__main__':
     
-    localsizes = [2**(i) for i in range(11)]
-    for j in localsizes:
-        print('With local group size %d'%j)
-        #initialize frameprocessor
-        t=time.time()
-        aproc = AlineProcessor(j)
-        print('Initialized in %.0fms: '%(1000*(time.time()-t)))
-        
-        nt=20000
-        intervals = []
-        results=[]
-        data = (np.load('data.npy')[:,:,0])
-        for i in range(nt):
-            t = time.time()
-            results.append(aproc.process_aline(np.ascontiguousarray(data[:,i])))
-            intervals.append(time.time()-t)
-            
-        print('Over %d alines:'%nt)
-        print('Average A-line rate: %.0fHz'%(1/(np.mean(intervals))))
-        print('Average A-line proc. time: %.2fms'%(1000*(np.mean(intervals))))
-        
+    data = np.load('data.npy')
+    data = data.flatten()
+    data = data[0:2048].astype(np.float32)
+    aproc = AlineProcessor(data)
+    times = []
+    result = (aproc.process_aline((data)))
         
     
